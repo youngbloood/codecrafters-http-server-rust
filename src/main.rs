53ae63bc4f6e0c2948b2a77984e38b11a1c3847a
jerad.k <xiankang.gao@enmotech.com>
2023-11-23 16:51:11 +0800

    2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
-
+use std::io::Write;
+// Uncomment this block to pass the first stage
+use std::net::TcpListener;
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
 
+    // Uncomment this block to pass the first stage
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                handle_tcpstream(&mut _stream);
+            Ok(mut stream) => {
+                println!("connection from {}", stream.peer_addr().unwrap());
+                stream
+                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                    .expect("write failed");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-}
-
-fn handle_tcpstream(ts: &mut TcpStream) {
-    let mut buffer = Vec::new();
-    // read the whole file
-    let _ = ts.read(&mut buffer);
-    // println!("buf = {:?}", buffer);
-    let resp = String::from("HTTP/1.1 200 OK\r\n\r\n");
-    let _ = ts.write_all(resp.as_bytes()).expect("write failed");
 }
