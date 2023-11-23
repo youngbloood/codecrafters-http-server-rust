c1e3a7afa3c3e9dd77e832c4836fbfb1483e389d
jerad.k <xiankang.gao@enmotech.com>
2023-11-23 16:38:12 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::net::TcpListener;
+use std::{
+    io::{self, Read},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                handle_tcpstream(&mut _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_tcpstream(ts: &mut TcpStream) {
+    let mut buffer = Vec::new();
+    // read the whole file
+    let _ = ts.read(&mut buffer);
+    println!("buf = {:?}", buffer);
 }
